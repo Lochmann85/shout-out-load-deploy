c5afb47585314ae9a6dc10ade000a7a422a8907f
georg lochmann <georg.lochmann@gmail.com>
2017-10-03 19:34:54 +0200

    test start

---
+++ server/build/configurations.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+   value: true
+});
+var TIMER_INTERVAL = 7000,
+   MAX_SHOWN_SHOUTS = 100;
+
+var serverConfig = {
+   OPENSHIFT_PORT: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+   OPENSHIFT_IP: process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
+   MONGO_DB_URI: process.env.MONGODB_SERVICE_HOST || "127.0.0.1",
+   MONGODB_SERVICE_PORT: process.env.MONGODB_SERVICE_PORT || "27017",
+   MONGO_DB_NAME: process.env.MONGODB_DATABASE || "solDb",
+   MONGO_USER_NAME: process.env.MONGODB_USER || "shout-out-loud",
+   MONGO_USER_PWD: process.env.MONGODB_PASSWORD || "storage"
+};
+
+exports.serverConfig = serverConfig;
+exports.TIMER_INTERVAL = TIMER_INTERVAL;
+exports.MAX_SHOWN_SHOUTS = MAX_SHOWN_SHOUTS;
---
+++ server/build/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var _configurations = require('./configurations');
+
+var express = require('express'),
+   app = express();
+
+app.get('/', function (req, res) {
+   res.send("test");
+});
+
+// error handling
+app.use(function (err, req, res, next) {
+   console.error(err.stack);
+   res.status(500).send('Something bad happened!');
+});
+
+app.listen(_configurations.OPENSHIFT_PORT, _configurations.OPENSHIFT_IP);
+
+module.exports = app;
